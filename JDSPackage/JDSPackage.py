fdda8bbade61672598f87775dcb7e447f478dec4
Jamaal <53444854+jdsmithwes@users.noreply.github.com>
2020-03-21 16:46:48 -0400

    Update JDSPackage.py
    header function with documentation

--- JDSPackage/JDSPackage.py
+++ JDSPackage/JDSPackage.py
@@ -2,12 +2,32 @@
 
 #Functions for header info in notebooks to automate including your name, date, and version number of a notebook
 
-#Header puts your name and current date
-
-def Header(name, project_name, date):
-    name = {'first': 'Jamaal', 'last': 'Smith'}
+def Header(name, project_name):
+    first_name = 'Jamaal'
+    last_name = 'Smith'
+    name = (first_name  , last_name)
     project_name = 'Flatiron School Capstone Project'
     date = datetime.today()
-    print(name['first'], name['last'])
+    print(name[0], name[1])
     print (project_name)
     print (date)
+    
+    """
+    
+    This is a quick way to create a header for your notebook
+    
+    Parameters:
+    
+    first_name (string): Input your first name
+    last_name (string): Input your last name
+    project_name (string): Place your project name here
+
+    Returns:
+
+    name: Print out of the first and last name of the notebook creator
+    project_name: Print out of the project name
+    date: Today's date
+    
+    """
+
+    
