c33aff4b617b80f69de0a647a33c58011898b837
Jamaal <53444854+jdsmithwes@users.noreply.github.com>
2020-03-22 10:28:31 -0400

    Update __init__.py
    copied function to see if it would help with py update

--- JDSPackage/__init__.py
+++ JDSPackage/__init__.py
@@ -3,3 +3,32 @@
 __author__ = """Jamaal Darius Smith"""
 __email__ = 'jdsmith1906@gmail.com'
 __version__ = '0.3.0'
+
+
+#Functions for header info in notebooks to automate including your name, date, and version number of a notebook
+
+def Header(name, project_name):
+    """
+    This is a quick way to create a header for your notebook.
+    
+    Parameters:
+    
+    first_name (string): Input your first name
+    last_name (string): Input your last name
+    project_name (string): Place your project name here
+
+    Returns:
+
+    name: Print out of the first and last name of the notebook creator
+    project_name: Print out of the project name
+    date: Today's date
+    
+    
+    """
+    first_name = 'Jamaal'
+    last_name = 'Smith'
+    name = (first_name  , last_name)
+    project_name = 'Flatiron School Capstone Project'
+    print(name[0], name[1])
+    print (project_name)
+    print (date)
