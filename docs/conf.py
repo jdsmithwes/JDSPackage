6c5b940d0f2e8b8d5e90a6480d9e9e358232d4f0
Jamaal <53444854+jdsmithwes@users.noreply.github.com>
2020-03-22 13:12:12 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -158,5 +158,17 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
+def run_apidoc(_):
+	from sphinx.ext.apidoc import main
+	import os
+	import sys
+	sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+	cur_dir = os.path.abspath(os.path.dirname(__file__))
+	module = os.path.join(cur_dir,"..","bs_ds")
+	main(['-o', cur_dir, module,'-M','--force'])
+
+def setup(app):
+	app.connect('builder-inited', run_apidoc)
+    
 
 
