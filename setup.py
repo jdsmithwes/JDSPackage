db4e7ca91511979538d3c6ba8cc13ef8ebf51985
Jamaal <53444854+jdsmithwes@users.noreply.github.com>
2020-03-22 11:38:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        #'Programming Language :: Python :: 3.8',
     ],
     description="documentation for first package",
     install_requires=requirements,
