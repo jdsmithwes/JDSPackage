27f00aa07d5d5813f9515198f331cddd41864eaf
Jamaal <53444854+jdsmithwes@users.noreply.github.com>
2020-03-22 13:23:51 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -44,6 +44,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jdsmithwes/JDSPackage',
-    version= 'version='0.5.2'',
+    version= '0.5.2',
     zip_safe= False,
 )
